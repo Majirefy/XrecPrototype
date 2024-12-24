c99e3a6e9f26065eff7fd6d1db4d1fe3d6d15d48
Majirefy <song.fc@outlook.com>
2024-12-24 19:13:57 +0800

    Initial Commit

---
+++ XrecPrototype/XrecPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XrecPrototypeApp.swift
+//  XrecPrototype
+//
+//  Created by Majirefy on 2024/12/24.
+//
+
+import SwiftUI
+
+@main
+struct XrecPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
